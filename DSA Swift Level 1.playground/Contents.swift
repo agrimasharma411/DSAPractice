74952787e033502f18e6cd0d291f5423252a326b
Agrima Sharma <agrimasharma411@gmail.com>
2025-07-02 17:27:14 +0530

    Swift remove duplicates qu

--- DSA Swift Level 1.playground/Contents.swift
+++ DSA Swift Level 1.playground/Contents.swift
@@ -8,12 +8,12 @@
 //   |                                         |
 //   |-----------------------------------------|
 
- var arr = [4,6,9,5,3,2]
- var max = -1
- for item in arr {
-     if (item > max ){
-         max = item
-     }
+// var arr = [4,6,9,5,3,2]
+// var max = -1
+// for item in arr {
+//     if (item > max ){
+//         max = item
+//     }
  }
  print (max)
 
@@ -23,22 +23,22 @@
 //   |                                         |
 //   |-----------------------------------------|
    
- var arr = [ 3,22,5,34,77,81,59,26]
- var even_count = 0
- var odd_count = 0
- for item in arr {
-     if(item % 2 == 0){
-     print ((item,"even"),terminator:"")
-       even_count += 1
-     }
-     else {
-     print((item,"=odd"),terminator:"")
-         odd_count += 1
-     }
- }
- print("\n")
- print ("Even count = \(even_count)")
- print ("Odd count = \(odd_count)")
+// var arr = [ 3,22,5,34,77,81,59,26]
+// var even_count = 0
+// var odd_count = 0
+// for item in arr {
+//     if(item % 2 == 0){
+//     print ((item,"even"),terminator:"")
+//       even_count += 1
+//     }
+//     else {
+//     print((item,"=odd"),terminator:"")
+//         odd_count += 1
+//     }
+// }
+// print("\n")
+// print ("Even count = \(even_count)")
+// print ("Odd count = \(odd_count)")
 
 //   |-----------------------------------------|
 //.  |                                         |
@@ -47,12 +47,12 @@
 //   |-----------------------------------------|
 
 // var arr = [1,2,3,4,5,6,7]
- var sum = 0
- for item in arr {
- sum = sum + item
- }
- print (sum)
-
+// var sum = 0
+// for item in arr {
+// sum = sum + item
+// }
+// print (sum)
+//
 
 //   |-----------------------------------------|
 //.  |                                         |
@@ -61,12 +61,12 @@
 //   |-----------------------------------------|
 
 // var arr = [31,98,41,74,51,47,61,71]
- var target = 47
- for (index,item) in arr.enumerated() {
-     if(item == target){
-         print("\(index)")
-     }
- }
+// var target = 47
+// for (index,item) in arr.enumerated() {
+//     if(item == target){
+//         print("\(index)")
+//     }
+// }
 
 //   |-----------------------------------------|
 //.  |                                         |
@@ -75,17 +75,17 @@
 //   |-----------------------------------------|
 
 // var arr = [47,89,23,89,51,87,51,87,78]
- var ans: [Int] = []
- for item1 in arr {
-   var freq = 0
- for item2 in arr {
-     if(item1 == item2 ) {
-         freq += 1
-     }
- }
- ans.append(freq)
- }
- print(ans)
+// var ans: [Int] = []
+// for item1 in arr {
+//   var freq = 0
+// for item2 in arr {
+//     if(item1 == item2 ) {
+//         freq += 1
+//     }
+// }
+// ans.append(freq)
+// }
+// print(ans)
 
 //   |-----------------------------------------|
 //.  |                                         |
@@ -108,20 +108,21 @@
 
 
 
-
-
-// var arr = [10, 20, 30, 40, 50]
-// var s = 0
-// var e = arr.count - 1
-// while s < e {
-//     let temp = arr[s]
-//     arr[s] = arr[e]
-//     arr[e] = temp
-    
-//     s += 1
-//     e -= 1
+//   |-----------------------------------------|
+//.  |                                         |
+//   |  QUESTION-7    Remove duplicates        |
+//   |                                         |
+//   |-----------------------------------------|
+//
+// var arr = [10,20,30,40,50,50,80]
+// var ans: [Int] = []
+// for item  in arr {
+//   if (!ans.contains(item)) {
+//     ans.append(item)
+//  }
 // }
-// print(arr)
+// print (ans)
+    
 
 
 
